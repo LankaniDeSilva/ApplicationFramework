5dae8c56a0200ba27801554becca001f85a04ae1
IT20221850 <IT20221850@my.sliit.lk>
2022-05-10 15:56:59 +0530

    Change the student Submission form

--- frontend/Component/Lankani/StudentFileAdd.js
+++ frontend/Component/Lankani/StudentFileAdd.js
@@ -1,69 +1,79 @@
 import React, { useState, set } from "react";
 import axios from "axios";
+import FormInput from "./SubmissionForm";
 
-export default function StudentFileSubmission(){
+export default function StudentFileSubmission() {
+  const [groupid, setgroupid] = useState("");
+  const [file, setfile] = useState("");
 
-    const [groupid, setgroupid] = useState("");
-    const [file, setfile] = useState("");
+  const onChangeGroupId = (e) => {
+    setgroupid(e.target.value);
+  };
 
-    const onChangeFile = (e) => {
-        setfile(e.target.files[0]);
-      };
+  const onChangeFile = (e) => {
+    setfile(e.target.files[0]);
+  };
 
-      const onChangeClick = (e) => {
-        e.preventDefault();
-    
-        const formdata = new FormData();
-    
-        formdata.append("groupid", groupid);
-        formdata.append("file", file);
-    
-        setgroupid("");
-        setfile("");
+  const onChangeClick = (e) => {
+    e.preventDefault();
 
-    
-        axios
-          .post("http://localhost:8001/file/save", formdata)
-          .then(() => {
-            alert("File is uploaded successfully");
-          })
-          .catch((err) => {
-            alert(err);
-          });
-      };
+    const formdata = new FormData();
 
-      return(
-          <div>
-            <div className="container">
-                <h1>Submit Document</h1>
-                    <form onSubmit={onChangeClick} encType="multipart/form-data">
-                        <div className="form-group">
-                            <label for="groupid">Group ID : </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="groupid"
-                                placeholder="Enter Group ID"
-                                onChange={(e) => {
-                                    setgroupid(e.target.value);
-                                }}
-                                />
-                        </div>
+    formdata.append("groupid", groupid);
+    formdata.append("file", file);
 
-                        <div className="form-group">
-                            <label for="filesub">File Submission : </label>
-                            <input
-                                type="file"
-                                className="form-control"
-                                id="filesub"
-                                filename="file"
-                                onChange={onChangeFile}
-                            />
-                        </div>
-                        <br/>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
-            </div>
-          </div>
-      )
+    setgroupid("");
+    setfile("");
+
+    axios
+      .post("http://localhost:8001/file/save", formdata)
+      .then(() => {
+        alert("File is uploaded successfully");
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  return (
+    <div
+      style={{
+        margin: "10%",
+        padding: "50px",
+        backgroundColor: "lightgrey",
+        borderColor: " solid",
+      }}
+    >
+      <div className="container">
+        <h2>Document Submission Form</h2>
+        <br />
+        <br />
+        <div className="form-group">
+          <FormInput
+            label="Group ID : "
+            name="groupid"
+            type="text"
+            onChange={onChangeGroupId}
+          />
+          <br />
+          <br />
+          <FormInput
+            label="File Submission : "
+            name="filesub"
+            type="file"
+            onChange={onChangeFile}
+          />
+          <br />
+          <br />
+          <button
+            type="submit"
+            onClick={onChangeClick}
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
